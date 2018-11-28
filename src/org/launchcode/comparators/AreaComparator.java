42a5ae14430cc9b2a26646d9f2416553f8d770e2
Sangeetha Rani <sangeetha.sabarinath@gmail.com>
2018-11-28 17:13:59 -0600

    Studio done but the bonus mission

---
+++ src/org/launchcode/comparators/AreaComparator.java
@@ -0,0 +1,13 @@
+package org.launchcode.comparators;
+
+import org.launchcode.City;
+
+import java.util.Comparator;
+
+public class AreaComparator implements Comparator<City> {
+
+    @Override
+    public int compare(City o1, City o2) {
+        return (int) (o1.getArea() - o2.getArea());
+    }
+}
